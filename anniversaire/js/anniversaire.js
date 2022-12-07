c166ec2cf8f2942f973d54d2a5f62e47e54c4df5
suzannelv <lvmeijuan9011@gmail.com>
2022-12-07 23:39:15 +0100

    style

--- anniversaire/js/anniversaire.js
+++ anniversaire/js/anniversaire.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
     $("h1").click(function(){
-        $(".en").show();
+        $(".en").show().addClass('bg-en');
     });
     $(".en").click(function() {
-        $(".fr").show();
+        $(".fr").show().addClass('bg-fr');
     });
     $(".fr").click(function() {
-        $(".cn").show();
+        $(".cn").show().addClass('bg-cn');
+    })
+    $('.cn').click(function() {
+        $('img').fadeIn(1000)
     })
    
 
 })
+//  $(document).ready(function () {
+//     $("h1").click(function(){
+//         $(".en").show().siblings().show().siblings().show()
+//     });
+//  })
