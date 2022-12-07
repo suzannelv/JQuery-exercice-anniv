58dc923801573f4c1a4266186f666da8eab0a752
suzannelv <lvmeijuan9011@gmail.com>
2022-12-07 17:06:09 +0100

    first commit

---
+++ anniversaire/js/anniversaire.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("h1").click(function(){
+        $(".en").show();
+    });
+    $(".en").click(function() {
+        $(".fr").show();
+    });
+    $(".fr").click(function() {
+        $(".cn").show();
+    })
+   
+
+})
